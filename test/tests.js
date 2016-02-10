e2f846bf55988ee1a6cfd89a546b41c31edde738
Marshall Bowers <elliott.codes@gmail.com>
2016-02-09 23:58:26 -0500

    Flatten promise chains in tests

--- test/tests.js
+++ test/tests.js
@@ -16,8 +16,8 @@ var sequelize = new Sequelize('sequelize_slugify_test', dbUsername, dbPassword, 
 });
 
 var SequelizeSlugify = require('../index');
-var chai = require("chai");
-var chaiAsPromised = require("chai-as-promised");
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 var expect = chai.expect;
 
@@ -34,12 +34,21 @@ var GetUser = function(modelname) {
         familyName: {
             type: Sequelize.STRING,
             allowNull: false
+        },
+        nickName: {
+            type: Sequelize.STRING,
+            allowNull: true
+        },
+        age: {
+            type: Sequelize.INTEGER,
+            allowNull: true
         }
     });
 };
 
 var User = {};
 var userId = 0;
+
 describe('sequelize-slugify', function () {
 
     describe('slugs', function () {
@@ -48,55 +57,119 @@ describe('sequelize-slugify', function () {
         beforeEach(function () {
             User = GetUser('user' + userId);
             userId++;
-            return sequelize.sync({force: true});
+
+            return sequelize.sync({ force: true });
         });
 
         it('should create a slug from a single field', function () {
             SequelizeSlugify.slugifyModel(User, {
                 source: ['givenName']
             });
-            return User.create({givenName: 'Suzan', familyName: 'Scheiber'})
-                .then(function (user) {
-                    return expect(user.slug).to.equal('suzan');
-                });
+
+            return User.create({
+                givenName: 'Suzan',
+                familyName: 'Scheiber'
+            }).then(function (user) {
+                return expect(user.slug).to.equal('suzan');
+            });
         });
 
         it('should create a slug from multiple fields', function () {
             SequelizeSlugify.slugifyModel(User, {
                 source: ['givenName', 'familyName']
             });
-            return User.create({givenName: 'Ernesto', familyName: 'Elsass'})
-                .then(function (user) {
-                    return expect(user.slug).to.equal('ernesto-elsass');
+
+            return User.create({
+                givenName: 'Ernesto',
+                familyName: 'Elsass'
+            }).then(function (user) {
+                return expect(user.slug).to.equal('ernesto-elsass');
+            });
+        });
+
+        describe('source suffixes', function () {
+            it('should use provided slug suffixes if the slug already exists', function () {
+                SequelizeSlugify.slugifyModel(User, {
+                    source: ['givenName', 'familyName'],
+                    suffixSource: ['nickName']
                 });
+
+                return User.create({
+                    givenName: 'Cleora',
+                    familyName: 'Curley'
+                }).then(function () {
+                    return User.create({
+                        givenName: 'Cleora',
+                        familyName: 'Curley',
+                        nickName: 'Cleo'
+                    });
+                }).then(function(user) {
+                    return expect(user.slug).to.equal('cleora-curley-cleo');
+                });
+            });
+
+            it('should handle multiple slug suffix fields', function () {
+                SequelizeSlugify.slugifyModel(User, {
+                    source: ['givenName', 'familyName'],
+                    suffixSource: ['nickName', 'age']
+                });
+
+                return User.create({
+                    givenName: 'Donald',
+                    familyName: 'Draper'
+                }).then(function () {
+                    return User.create({
+                        givenName: 'Donald',
+                        familyName: 'Draper',
+                        nickName: 'Don'
+                    });
+                }).then(function () {
+                    return User.create({
+                        givenName: 'Donald',
+                        familyName: 'Draper',
+                        nickName: 'Don',
+                        age: 42
+                    });
+                }).then(function(user) {
+                    return expect(user.slug).to.equal('donald-draper-don-42');
+                });
+            });
         });
 
         it('should increment slug suffix if it already exists', function () {
             SequelizeSlugify.slugifyModel(User, {
                 source: ['givenName', 'familyName']
             });
-            return User.create({givenName: 'Cleora', familyName: 'Curley'})
-                .then(function () {
-                    return User.create({givenName: 'Cleora', familyName: 'Curley'})
-                        .then(function(user) {
-                            return expect(user.slug).to.equal('cleora-curley-1');
-                        });
+
+            return User.create({
+                givenName: 'Cleora',
+                familyName: 'Curley'
+            }).then(function () {
+                return User.create({
+                    givenName: 'Cleora',
+                    familyName: 'Curley'
                 });
+            }).then(function(user) {
+                return expect(user.slug).to.equal('cleora-curley-1');
+            });
         });
 
         it('should overwrite slug by default', function () {
             SequelizeSlugify.slugifyModel(User, {
                 source: ['givenName']
             });
-            return User.create({givenName: 'Rupert', familyName: 'Rinaldi'})
-                .then(function (user) {
-                    user.givenName = 'Genie';
-                    user.familyName = 'Gayden';
-                    return user.save()
-                        .then(function(updatedUser) {
-                            return expect(updatedUser.slug).to.equal('genie');
-                        });
-                });
+
+            return User.create({
+                givenName: 'Rupert',
+                familyName: 'Rinaldi'
+            }).then(function (user) {
+                user.givenName = 'Genie';
+                user.familyName = 'Gayden';
+
+                return user.save();
+            }).then(function(updatedUser) {
+                return expect(updatedUser.slug).to.equal('genie');
+            });
         });
 
         it('should NOT overwrite slug if option says not to', function () {
@@ -104,15 +177,18 @@ describe('sequelize-slugify', function () {
                 source: ['givenName'],
                 overwrite: false
             });
-            return User.create({givenName: 'Miquel', familyName: 'Mceachin'})
-                .then(function (user) {
-                    user.givenName = 'Sallie';
-                    user.familyName = 'Shira';
-                    return user.save()
-                        .then(function(updatedUser) {
-                            return expect(updatedUser.slug).to.equal('miquel');
-                        });
-                });
+
+            return User.create({
+                givenName: 'Miquel',
+                familyName: 'Mceachin'
+            }).then(function (user) {
+                user.givenName = 'Sallie';
+                user.familyName = 'Shira';
+
+                return user.save();
+            }).then(function(updatedUser) {
+                return expect(updatedUser.slug).to.equal('miquel');
+            });
         });
     });
 
