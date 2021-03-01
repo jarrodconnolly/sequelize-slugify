8d106600708893634f504870dd6e3033afc29509
Jarrod Connolly <jarrod@nestedquotes.ca>
2021-02-28 20:28:38 -0800

    ci: further organize of ci workflow

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,13 @@ module.exports = {
   testMatch: ["**/jest-tests/**/*.test.js"],
   collectCoverageFrom: ["<rootDir>/lib/sequelize-slugify.js"],
   coverageDirectory: "coverage",
-  coverageReporters: ["lcov", "text"]
+  coverageReporters: ["lcov", "text"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  }
 };
