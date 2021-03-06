b35750284b3b1ce2a1fa56a637dbad6315c73ea9
Jarrod Connolly <jarrod@nestedquotes.ca>
2021-03-05 21:56:35 -0800

    feat: support for optional bulk update slug regeneration

--- docs/options.md
+++ docs/options.md
@@ -13,7 +13,8 @@ SequelizeSlugify.slugifyModel(User, {
     column: 'slug',
     incrementalSeparator: '-',
     passTransaction: true,
-    paranoid: true
+    paranoid: true,
+    bulkUpdate: false
 });
 ```
 ### Available Options
@@ -24,10 +25,12 @@ SequelizeSlugify.slugifyModel(User, {
 | `slugOptions`          | `{}` |          | `{lower:true}` | Pass additional options for slug generation as defined by [`sluglife`][SL] |
 | `overwrite`            | `boolean` |     | `true` | Update the slug if the source fields change after initial generation |
 | `column`               | `string` |      | `slug` | Specify the column to store the slug value |
-| `incrementalSeparator` |          |      | `-`    | Specify the separator between the slug, and the duplicate count |
+| `incrementalSeparator` | `string` |      | `-`    | Specify the separator between the slug, and the duplicate count |
 | `passTransaction`      | `boolean` |     | `true` | Pass the current transaction object in to the plugin |
 | `paranoid`             | `boolean` |     | `true` | Whether the duplication check will use a paranoid query or not, for determining the next unique slug. |
-
+| `bulkUpdate`           | `boolean` |     | `false` | Automatically enable `individualHooks` during bulk updates. Read the following information: [Note-1][N1] [Note-2][N2] |
 
 
 [SL]: https://github.com/jarrodconnolly/sluglife#options "Slug Life Options"
+[N1]: https://sequelize.org/master/manual/hooks.html#model-hooks "individualHooks Note 1"
+[N2]: https://sequelize.org/master/manual/hooks.html#many-to-many-associations "individualHooks Node 2"
--- jest-tests/slug-bulk-update.test.js
+++ jest-tests/slug-bulk-update.test.js
@@ -3,12 +3,10 @@
 const SequelizeSlugify = require('../index');
 
 let User;
-// let userData;
 
 describe('bulk update', () => {
   beforeEach(async () => {
     User = await global.generateModel();
-    // userData = global.createUser();
   });
 
   it('should create a slug during a bulk create', async () => {
@@ -23,26 +21,38 @@ describe('bulk update', () => {
     expect(results[0].slug).toBe(`${user1.givenName.toLowerCase()}`);
     expect(results[1].slug).toBe(`${user2.givenName.toLowerCase()}`);
   });
+
+  it('should bulk update a slug from the Model', async ()=> {
+    SequelizeSlugify.slugifyModel(User, {
+      source: ['givenName'],
+      bulkUpdate: true
+    });
+
+    const userData = global.createUser();
+    await User.create(userData);
+    const newGivenName = global.generateGivenName();
+
+    await User.update({givenName: newGivenName}, {where: {givenName: userData.givenName}});
+
+    const foundUser = await User.findOne({where:{givenName:newGivenName}});
+    expect(foundUser.slug).toBe(newGivenName.toLowerCase());
+  });
+
+  it('should bulk update with manual individualHooks', async ()=> {
+    SequelizeSlugify.slugifyModel(User, {
+      source: ['givenName'],
+    });
+
+    const userData = global.createUser();
+    await User.create(userData);
+    const newGivenName = global.generateGivenName();
+
+    await User.update({givenName: newGivenName}, {
+      where: {givenName: userData.givenName},
+      individualHooks: true
+    });
+
+    const foundUser = await User.findOne({where:{givenName:newGivenName}});
+    expect(foundUser.slug).toBe(newGivenName.toLowerCase());
+  });
 });
-
-// xit('should bulk update a slug from the Model', function () {
-//   SequelizeSlugify.slugifyModel(User, {
-//     source: ['givenName'],
-//   });
-//
-//   return User.create({
-//     givenName: 'Woibrert',
-//     familyName: 'Hamazoni',
-//   }).then(function () {
-//     return User.update({
-//       givenName: 'Hazzah',
-//     }, {where: {givenName: 'Woibrert'}});
-//   }).then(function () {
-//     return User.findOne({givenName: 'Hazzah'});
-//   }).then(function (user) {
-//     return expect(user.slug).to.equal('hazzah');
-//   });
-// });
-//
-
-// });
