e6a299ed64c2b3dc2670ed75dc1f10cd53a3394a
Jarrod Connolly <jarrod@nestedquotes.ca>
2021-03-10 00:40:26 -0800

    chore: move to my own shared eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,5 @@
 'use strict';
 
 module.exports = {
-  'env': {
-    'commonjs': true,
-    'es6': true,
-    'node': true,
-    'jest': true,
-    'es2021': true
-  },
-  'extends': 'eslint:recommended',
-  'parserOptions': {
-    'ecmaVersion': 12,
-    'sourceType': "script",
-  },
-  'rules': {
-    "semi": "error"
-  },
+  'extends': 'eslint-config-jarrod',
 };
